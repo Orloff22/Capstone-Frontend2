39f620f3f83331b10e75de931fafc704115bc345
Orloff22 <sarah.orloff.hamilton@gmail.com>
2022-07-26 07:16:20 -0400

    first change

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,17 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AliasComponent } from './alias/alias.component';
+import { ConfigComponent } from './config/config.component';
+import { HomeComponent } from './home/home.component';
+import { ReportsComponent } from './reports/reports.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'home', component: HomeComponent},
+  {path: 'reports', component: ReportsComponent},
+  {path:'alias', component: AliasComponent},
+  {path:'config', component: ConfigComponent},
+  {path:'', redirectTo: './home', pathMatch: 'full'}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/reports/reports.component.ts
+++ src/app/reports/reports.component.ts
@@ -1,36 +1,15 @@
 import { Component, OnInit } from '@angular/core';
-import { Employee } from '../employee';
-import { EmployeeService } from '../employee.service';
-import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-reports',
   templateUrl: './reports.component.html',
-  styleUrls: ['.reports.component.css']
+  styleUrls: ['./reports.component.css']
 })
 export class ReportsComponent implements OnInit {
 
-  employee: Employee = new Employee();
-  constructor(private employeeService: EmployeeService,
-    private router: Router) { }
+  constructor() { }
 
   ngOnInit(): void {
   }
 
-  saveEmployee(){
-    this.employeeService.createEmployee(this.employee).subscribe( data =>{
-      console.log(data);
-      this.goToEmployeeList();
-    },
-    error => console.log(error));
-  }
-
-  goToEmployeeList(){
-    this.router.navigate(['/employees']);
-  }
-  
-  onSubmit(){
-    console.log(this.employee);
-    this.saveEmployee();
-  }
 }
